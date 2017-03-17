9ac9d5f88a38d2f383df048c39ca44c5b8eb0847
mwei2509 <m2dl89@gmail.com>
2017-03-17 17:34:18 -0400

    randomizer

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-var sounds=['sound1', 'sound2', 'sound3']
+var sounds=['sound1', 'sound2', 'sound3', 'sound4', 'sound5','sound6','sound7','sound8','sound9']
   $(document).on('keydown', function(){
     document.getElementById(sounds[Math.round(Math.random()*sounds.length)]).play();
   })
