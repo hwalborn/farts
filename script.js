64daaa96bf80b3a1d0a5b8bfbc6ebf672ce316a9
hwalborn <hwalborn@gmail.com>
2017-03-17 18:29:05 -0400

    added more farts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(document).on('keydown', function() {
-    const num_farts = 9
+    const num_farts = 12
     function random_fart_id(){
       return Math.floor(Math.random()*num_farts)+1
     }
