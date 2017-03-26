43a0cddf95db504959591da22c425dd9f8369ef1
hwalborn <hwalborn@gmail.com>
2017-03-26 13:50:06 -0400

    It works

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-$(document).ready(function(){
-  $(document).on('keydown', function() {
-    const num_farts = 11
-    function random_fart_id(){
-      return Math.floor(Math.random()*num_farts)+1
-    }
-    document.getElementById(`sound${random_fart_id()}`).play();
-  })
-})
 
-function playSoundAsync(url){
-    new Audio(url).play();
-}
+
+(() => {
+    $(document).ready(function() {
+        $(document).on('keydown', function() {
+            const num_farts = 11
+            function random_fart_id() {
+                return Math.floor(Math.random() * num_farts) + 1
+            }
+            var url = chrome.runtime.getURL(`fart-noises/fart-${random_fart_id()}.wav`)
+            var audio = new Audio(url)
+            audio.play()
+        })
+    })
+})();
